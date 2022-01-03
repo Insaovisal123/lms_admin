1c8363cba1230c4101207cd103a55baefd063c5b
kim <kimheang135@gmail.com>
2022-01-03 22:54:40 +0700

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - crm-backend',
-    title: 'crm-backend',
+    titleTemplate: '%s - Backend',
+    title: 'LMS',
     htmlAttrs: {
       lang: 'en'
     },
